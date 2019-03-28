135de318c1093c4be02ec5c517d05aa7fd628b29
muhammadfaiznoh <muhammadfaiznoh@gmail.com>
2019-03-28 14:35:01 +0800

    few design change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -42,17 +42,18 @@ export class HomePage {
  
   getBarChart() {
     const data = {
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun'],
       datasets: [{
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 19, 3, 5, 2, 3,20],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 0, 64, 0.2)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
@@ -60,7 +61,8 @@ export class HomePage {
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 0, 64, 0.2)'
         ],
         borderWidth: 1
       }]
